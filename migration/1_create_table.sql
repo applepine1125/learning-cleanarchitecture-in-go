2f6388585a5f36eca4cd52d5235f75e5a1b24429
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2019-02-25 00:35:48 +0900

    init

---
+++ migration/1_create_table.sql
@@ -0,0 +1,6 @@
+REATE TABLE users (
+    id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ migration/2_insert_data.sql
@@ -0,0 +1,5 @@
+insert into users(first_name, last_name) values("Patricia", "Smith");
+insert into users(first_name, last_name) values("Linda", "Johnson");
+insert into users(first_name, last_name) values("Mary", "William");
+insert into users(first_name, last_name) values("Robert", "Jones");
+insert into users(first_name, last_name) values("James", "Brown");
