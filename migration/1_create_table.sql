9d07aad62f00b39540facd0c16afe1e75779cccb
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2019-02-25 09:11:46 +0900

    add task.go, fix create tasks table query

--- migration/1_create_table.sql
+++ migration/1_create_table.sql
@@ -8,8 +8,8 @@ CREATE TABLE users (
 CREATE TABLE tasks (
     id INTEGER unsigned NOT NULL AUTO_INCREMENT,
     user_id INTEGER unsigned NOT NULL, 
-    title VARCHAR(20) NOT NULL,
-    description VARCHAR(20) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(200) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) 
         REFERENCES users(id)
---
+++ src/pkg/domain/task.go
@@ -0,0 +1,8 @@
+package domain
+
+type tasks struct {
+	ID          int64
+	UserID      int64
+	Title       string
+	Description string
+}
