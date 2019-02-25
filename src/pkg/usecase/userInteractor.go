a1662cc38211b6bb6afa40aa4f5e2c1d925df1ba
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2019-02-25 09:25:31 +0900

    add userInteractor.go

---
+++ src/pkg/usecase/userInteractor.go
@@ -0,0 +1,20 @@
+package usecase
+
+import "github.com/applepine1125/learning-cleanarchitecture-in-go/src/pkg/domain"
+
+type UserInteractor struct {
+	userRepo Userrepository
+	taskRepo TaskRepository
+}
+
+func (ui *UserInteractor) Add(u domain.User) error {
+	return ui.userRepo.Store(u)
+}
+
+func (ui *UserInteractor) FindById(id int) (domain.User, error) {
+	return ui.FindById(id)
+}
+
+func (ui *UserInteractor) FindAll() (domain.Users, error) {
+	return ui.userRepo.FindAll()
+}
