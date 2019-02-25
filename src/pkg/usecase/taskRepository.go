ff3e248b5fa15b0480bf1206ac17dd1b3e42af94
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2019-02-25 23:16:44 +0900

    fix taskRepository.go

--- src/pkg/usecase/taskRepository.go
+++ src/pkg/usecase/taskRepository.go
@@ -1,9 +1,7 @@
 package usecase
 
-import "github.com/applepine1125/learning-cleanarchitecture-in-go/src/pkg/domain"
-
 type TaskRepository interface {
-	Store(domain.Task) error
-	FindById(int) (domain.Task, error)
-	FindAll() (domain.Tasks, error)
+	Store(Task) error
+	FindById(int) (Task, error)
+	FindAll() (Tasks, error)
 }
