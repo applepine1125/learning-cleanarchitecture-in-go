94f546ac8bff9fd7ffe99cf4b0f2a46261f61ef4
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2019-02-28 09:33:48 +0900

    add taskcontroler in router

--- src/pkg/infra/router.go
+++ src/pkg/infra/router.go
@@ -13,9 +13,14 @@ func init() {
 
 	userController := controller.NewUserController(NewSqlHandler())
 
+	taskController := controller.NewTaskController(NewSqlHandler())
+
 	router.POST("/users", func(c *gin.Context) { userController.Create(c) })
 	router.GET("/users/:id", func(c *gin.Context) { userController.GetUser(c) })
 	router.GET("/users", func(c *gin.Context) { userController.GetUsers(c) })
 
+	router.POST("/tasks", func(c *gin.Context) { taskController.Create(c) })
+	router.GET("/tasks/:id", func(c *gin.Context) { taskController.GetTask(c) })
+	router.GET("/tasks", func(c *gin.Context) { taskController.GetTasks(c) })
 	Router = router
 }
