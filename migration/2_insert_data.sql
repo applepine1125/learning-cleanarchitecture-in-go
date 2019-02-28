658b980963e39a07ea82d0fb6f338e4ef3c29afa
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2019-02-28 09:40:17 +0900

    fix migration file

--- migration/2_insert_data.sql
+++ migration/2_insert_data.sql
@@ -3,3 +3,8 @@ insert into users(first_name, last_name) values("Linda", "Johnson");
 insert into users(first_name, last_name) values("Mary", "William");
 insert into users(first_name, last_name) values("Robert", "Jones");
 insert into users(first_name, last_name) values("James", "Brown");
+
+insert into tasks(user_id, title, description) values(1, "task1", "aaaaaa");
+insert into tasks(user_id, title, description) values(2, "task2", "bbbbbb");
+insert into tasks(user_id, title, description) values(3, "task3", "cccccc");
+insert into tasks(user_id, title, description) values(4, "task4", "dddddd");
